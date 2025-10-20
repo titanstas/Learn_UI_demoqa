52670a15f327796a71a66f5f4eaddd739f1410da
Stas_Makarov <titanstas@yandex.ru>
2025-10-20 18:46:03 +0300

    Добавил testng1.xml, добавил группы во все тесты

--- src/test/java/demoqa/Tests_demoqa.java
+++ src/test/java/demoqa/Tests_demoqa.java
@@ -14,7 +14,7 @@ import pages.demoqa.elements.*;
 
 public class Tests_demoqa extends BaseTests{
 
-    @Test
+    @Test (groups = {"smoke"})
     public void test_go () throws InterruptedException {
         Demoqa_main_page mainPage = new Demoqa_main_page(driver);
         driver.get(Demoqa_main_page.demoqa_host);
@@ -25,7 +25,7 @@ public class Tests_demoqa extends BaseTests{
 
 
     @Test (dataProvider = "text_box_provider", dataProviderClass = Data_provider_text_box.class,
-            groups = {"text_box"})
+            groups = {"text_box","sanity"})
 
     public void test_text_box_1 (String full_name, String email, String current_address, String permanent_address ) throws InterruptedException {
         Text_box_page page = new Text_box_page(driver);
@@ -39,7 +39,7 @@ public class Tests_demoqa extends BaseTests{
     }
 
     @Test (dataProvider = "text_box_provider_object", dataProviderClass = Data_provider_text_box.class,
-            groups = {"text_box"})
+            groups = {"text_box","smoke"})
     @Tag("text_box")
     public void test_text_box_2 (Text_box_object object) throws InterruptedException {
         Text_box_page page = new Text_box_page(driver);
@@ -49,7 +49,7 @@ public class Tests_demoqa extends BaseTests{
         Thread.sleep(1000);
     }
 
-    @Test (groups = {"check_box"})
+    @Test (groups = {"check_box","sanity"})
     public void test_check_box_1 () throws InterruptedException {
         Check_box_page page = new Check_box_page(driver);
         driver.get(Check_box_page.demoqa_checkbox_host);
@@ -63,7 +63,7 @@ public class Tests_demoqa extends BaseTests{
 
     }
 
-    @Test
+    @Test(groups = {"check_box","smoke"})
     public void test_check_box_2 () throws InterruptedException {
         Check_box_page page = new Check_box_page(driver);
         driver.get(Check_box_page.demoqa_checkbox_host);
@@ -75,7 +75,7 @@ public class Tests_demoqa extends BaseTests{
 
     }
 
-    @Test
+    @Test (groups = {"radio_button","sanity"})
     public void test_radio_button_1 () throws InterruptedException {
         Radio_button_page page = new Radio_button_page(driver);
         driver.get(Radio_button_page.demoqa_radio_button_host);
@@ -88,7 +88,7 @@ public class Tests_demoqa extends BaseTests{
 
     }
 
-    @Test
+    @Test (groups = {"radio_button","sanity"})
     public void test_radio_button_cond () throws InterruptedException {
         Radio_button_page page = new Radio_button_page(driver);
         driver.get(Radio_button_page.demoqa_radio_button_host);
@@ -96,70 +96,70 @@ public class Tests_demoqa extends BaseTests{
         page.press_radio_button_yes_2();
     }
 
-    @Test (dataProvider = "web_tables_provider_object", dataProviderClass = Data_provider_web_tables.class)
+    @Test (dataProvider = "web_tables_provider_object", dataProviderClass = Data_provider_web_tables.class, groups = {"web_tables","smoke"} )
     public void test_add_person_in_web_table (Web_tables_object object) throws InterruptedException {
         Web_tables_page page = new Web_tables_page(driver);
         driver.get(Web_tables_page.demoqa_webtables_host);
         page.add_person_in_web_table(object);
     }
 
-    @Test (dataProvider = "web_tables_provider_object_edit", dataProviderClass = Data_provider_web_tables.class)
+    @Test (dataProvider = "web_tables_provider_object_edit", dataProviderClass = Data_provider_web_tables.class, groups = {"web_tables","smoke"})
     public void test_edit_person_in_web_table (Web_tables_object object, String person_row_number) throws InterruptedException {
         Web_tables_page page = new Web_tables_page(driver);
         driver.get(Web_tables_page.demoqa_webtables_host);
         page.edit_person_in_web_table(object, person_row_number);
     }
 
-    @Test (dataProvider = "web_tables_provider_object_delete", dataProviderClass = Data_provider_web_tables.class)
+    @Test (dataProvider = "web_tables_provider_object_delete", dataProviderClass = Data_provider_web_tables.class, groups = {"web_tables","smoke"})
     public void test_delete_person_in_web_table ( String person_row_number)  {
         Web_tables_page page = new Web_tables_page(driver);
         driver.get(Web_tables_page.demoqa_webtables_host);
         page.delete_person_in_web_table( person_row_number);
     }
 
-    @Test (dataProvider = "web_tables_provider_object", dataProviderClass = Data_provider_web_tables.class)
-    public void test (Web_tables_object object) throws InterruptedException {
+    @Test (dataProvider = "web_tables_provider_object", dataProviderClass = Data_provider_web_tables.class, groups = {"web_tables","sanity"})
+    public void test_search (Web_tables_object object) throws InterruptedException {
         Web_tables_page page = new Web_tables_page(driver);
         driver.get(Web_tables_page.demoqa_webtables_host);
         page.search(object.getFirst_name());
     }
 
-    @Test (dataProvider = "web_tables_provider_object", dataProviderClass = Data_provider_web_tables.class)
+    @Test (dataProvider = "web_tables_provider_object", dataProviderClass = Data_provider_web_tables.class, groups = {"web_tables","smoke"})
     public void test_add_search_get_person_in_web_table (Web_tables_object object) throws InterruptedException {
         Web_tables_page page = new Web_tables_page(driver);
         driver.get(Web_tables_page.demoqa_webtables_host);
         page.add_search_get_person_in_web_table(object);
     }
 
-    @Test (dataProvider = "web_tables_provider_object", dataProviderClass = Data_provider_web_tables.class)
+    @Test (dataProvider = "web_tables_provider_object", dataProviderClass = Data_provider_web_tables.class, groups = {"web_tables","sanity"})
     public void _get_first_names_in_web_table (Web_tables_object object) throws InterruptedException {
         Web_tables_page page = new Web_tables_page(driver);
         driver.get(Web_tables_page.demoqa_webtables_host);
         page.get_first_names_in_table();
     }
 
-    @Test ()
+    @Test ( groups = {"web_tables","sanity"})
     public void get_persons_in_web_table ()  {
         Web_tables_page page = new Web_tables_page(driver);
         driver.get(Web_tables_page.demoqa_webtables_host);
         page.get_persons_in_table();
     }
 
-    @Test ()
+    @Test (groups = {"buttons","sanity"})
     public void press_double_click_me_button ()  {
         Buttons_page page = new Buttons_page(driver);
         driver.get(Buttons_page.demoqa_buttons_page_host);
         page.press_double_click_me_button();
     }
 
-    @Test ()
+    @Test (groups = {"buttons","sanity"})
     public void press_right_click_me_button ()  {
         Buttons_page page = new Buttons_page(driver);
         driver.get(Buttons_page.demoqa_buttons_page_host);
         page.press_right_click_me_button();
     }
 
-    @Test ()
+    @Test (groups = {"buttons","smoke"})
     public void click_all_button ()  {
         Buttons_page page = new Buttons_page(driver);
         driver.get(Buttons_page.demoqa_buttons_page_host);
@@ -167,7 +167,7 @@ public class Tests_demoqa extends BaseTests{
 
     }
 
-    @Test ()
+    @Test (groups = {"links","smoke"})
     public void goto_main_page_link_from_links ()  {
         Links_page page = new Links_page(driver);
         driver.get(Links_page.demoqa_links_page_host);
